543b62a41debb75a606fe8fd6bb89de937f8af5e
Armensch11 <60622581+Armensch11@users.noreply.github.com>
2020-05-25 03:51:12 +0400

    Add files via upload

--- script/script-using-objects-for-shapes.js
+++ script/script-using-objects-for-shapes.js
@@ -29,7 +29,11 @@
 	startPos.appendChild(start);
 	start = document.getElementById('start');
 	// start.innerHTML = 'start';
-	start.addEventListener('click', () => entryPoint());
+	start.addEventListener('click', () => {
+		triangleObj = createTriangle();
+		let stop = setInterval(() => triangleObj.moveDown(), 800);
+		setTimeout(clearInterval, 10000, stop);
+	});
 	const stopPos = document.getElementById('stop-place');
 	let stop = document.createElement('div');
 	stop.setAttribute('id', 'stop');
@@ -116,7 +120,8 @@ function clearCells() {
 		cell.setAttribute('data-empty', 'yes');
 	}
 }
-let triangleObj = (function(x = 3, y = 2, shapeTriangle = {}) {
+let triangleObj = createTriangle();
+function createTriangle(x = 3, y = 2, shapeTriangle = {}) {
 	shapeTriangle.position = 1;
 	shapeTriangle.startRow = document.querySelector(`[data-y="${y}"]`);
 	shapeTriangle.yCord = +shapeTriangle.startRow.getAttribute('data-y');
@@ -407,27 +412,28 @@ let triangleObj = (function(x = 3, y = 2, shapeTriangle = {}) {
 			let y_p1 = this.point1.getAttribute('data-y');
 			let p1_under = document.querySelectorAll(`[data-x='${x_p1}']`)[y_p1];
 			if (p2_under && p4_under) {
-				if (p4_under.getAttribute('data-empty') === 'yes' && p2_under.getAttribute('data-empty') === 'yes')
+				if (p4_under.getAttribute('data-empty') === 'yes' && p2_under.getAttribute('data-empty') === 'yes') {
 					this.point2.setAttribute('data-empty', 'yes');
-				this.point2.style.backgroundColor = null;
-				this.point2 = p2_under;
-				this.point2.setAttribute('data-empty', 'no');
-				this.point2.style.backgroundColor = '#874da8';
-				this.point4.setAttribute('data-empty', 'yes');
-				this.point4.style.backgroundColor = null;
-				this.point4 = p4_under;
-				this.point4.setAttribute('data-empty', 'no');
-				this.point4.style.backgroundColor = '#874da8';
-				this.point1.setAttribute('data-empty', 'yes');
-				this.point1.style.backgroundColor = null;
-				this.point1 = p1_under;
-				this.point1.setAttribute('data-empty', 'no');
-				this.point1.style.backgroundColor = '#874da8';
-				this.point3.setAttribute('data-empty', 'yes');
-				this.point3.style.backgroundColor = null;
-				this.point3 = p3_under;
-				this.point3.setAttribute('data-empty', 'no');
-				this.point3.style.backgroundColor = '#874da8';
+					this.point2.style.backgroundColor = null;
+					this.point2 = p2_under;
+					this.point2.setAttribute('data-empty', 'no');
+					this.point2.style.backgroundColor = '#874da8';
+					this.point4.setAttribute('data-empty', 'yes');
+					this.point4.style.backgroundColor = null;
+					this.point4 = p4_under;
+					this.point4.setAttribute('data-empty', 'no');
+					this.point4.style.backgroundColor = '#874da8';
+					this.point1.setAttribute('data-empty', 'yes');
+					this.point1.style.backgroundColor = null;
+					this.point1 = p1_under;
+					this.point1.setAttribute('data-empty', 'no');
+					this.point1.style.backgroundColor = '#874da8';
+					this.point3.setAttribute('data-empty', 'yes');
+					this.point3.style.backgroundColor = null;
+					this.point3 = p3_under;
+					this.point3.setAttribute('data-empty', 'no');
+					this.point3.style.backgroundColor = '#874da8';
+				}
 			}
 		}
 	};
@@ -594,182 +600,182 @@ let triangleObj = (function(x = 3, y = 2, shapeTriangle = {}) {
 				}
 			}
 		}
-		shapeTriangle.moveRight = function() {
-			if (this.position === 1) {
-				// debugger;
-				let x_p4 = +this.point4.getAttribute('data-x');
-				let y_p4 = +this.point4.getAttribute('data-y');
-				let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
-				let x_p2 = +this.point2.getAttribute('data-x');
-				let y_p2 = +this.point2.getAttribute('data-y');
-				let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
-				let x_p3 = +this.point3.getAttribute('data-x');
-				let y_p3 = +this.point3.getAttribute('data-y');
-				let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
-				let x_p1 = +this.point1.getAttribute('data-x');
-				let y_p1 = +this.point1.getAttribute('data-y');
-				let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
-				if (p3_toRight && p4_toRight) {
-					if (
-						p3_toRight.getAttribute('data-empty') === 'yes' &&
-						p4_toRight.getAttribute('data-empty') === 'yes'
-					) {
-						this.point4.setAttribute('data-empty', 'yes');
-						this.point4.style.backgroundColor = null;
-						this.point4 = p4_toRight;
-						this.point4.setAttribute('data-empty', 'no');
-						this.point4.style.backgroundColor = '#874da8';
-						this.point3.setAttribute('data-empty', 'yes');
-						this.point3.style.backgroundColor = null;
-						this.point3 = p3_toRight;
-						this.point3.setAttribute('data-empty', 'no');
-						this.point3.style.backgroundColor = '#874da8';
-						this.point1.setAttribute('data-empty', 'yes');
-						this.point1.style.backgroundColor = null;
-						this.point1 = p1_toRight;
-						this.point1.setAttribute('data-empty', 'no');
-						this.point1.style.backgroundColor = '#874da8';
+	};
+	shapeTriangle.moveRight = function() {
+		if (this.position === 1) {
+			// debugger;
+			let x_p4 = +this.point4.getAttribute('data-x');
+			let y_p4 = +this.point4.getAttribute('data-y');
+			let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
+			let x_p2 = +this.point2.getAttribute('data-x');
+			let y_p2 = +this.point2.getAttribute('data-y');
+			let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
+			let x_p3 = +this.point3.getAttribute('data-x');
+			let y_p3 = +this.point3.getAttribute('data-y');
+			let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
+			let x_p1 = +this.point1.getAttribute('data-x');
+			let y_p1 = +this.point1.getAttribute('data-y');
+			let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
+			if (p3_toRight && p4_toRight) {
+				if (
+					p3_toRight.getAttribute('data-empty') === 'yes' &&
+					p4_toRight.getAttribute('data-empty') === 'yes'
+				) {
+					this.point4.setAttribute('data-empty', 'yes');
+					this.point4.style.backgroundColor = null;
+					this.point4 = p4_toRight;
+					this.point4.setAttribute('data-empty', 'no');
+					this.point4.style.backgroundColor = '#874da8';
+					this.point3.setAttribute('data-empty', 'yes');
+					this.point3.style.backgroundColor = null;
+					this.point3 = p3_toRight;
+					this.point3.setAttribute('data-empty', 'no');
+					this.point3.style.backgroundColor = '#874da8';
+					this.point1.setAttribute('data-empty', 'yes');
+					this.point1.style.backgroundColor = null;
+					this.point1 = p1_toRight;
+					this.point1.setAttribute('data-empty', 'no');
+					this.point1.style.backgroundColor = '#874da8';
 
-						this.point2.setAttribute('data-empty', 'yes');
-						this.point2.style.backgroundColor = null;
-						this.point2 = p2_toRight;
-						this.point2.setAttribute('data-empty', 'no');
-						this.point2.style.backgroundColor = '#874da8';
-					}
+					this.point2.setAttribute('data-empty', 'yes');
+					this.point2.style.backgroundColor = null;
+					this.point2 = p2_toRight;
+					this.point2.setAttribute('data-empty', 'no');
+					this.point2.style.backgroundColor = '#874da8';
 				}
 			}
-			if (this.position === 2) {
-				let x_p4 = +this.point4.getAttribute('data-x');
-				let y_p4 = +this.point4.getAttribute('data-y');
-				let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
-				let x_p2 = +this.point2.getAttribute('data-x');
-				let y_p2 = +this.point2.getAttribute('data-y');
-				let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
-				let x_p3 = +this.point3.getAttribute('data-x');
-				let y_p3 = +this.point3.getAttribute('data-y');
-				let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
-				let x_p1 = +this.point1.getAttribute('data-x');
-				let y_p1 = +this.point1.getAttribute('data-y');
-				let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
-				if (p1_toRight && p2_toRight && p3_toRight) {
-					if (
-						p1_toRight.getAttribute('data-empty') === 'yes' &&
-						p2_toRight.getAttribute('data-empty') === 'yes' &&
-						p3_toRight.getAttribute('data-empty') === 'yes'
-					) {
-						this.point1.setAttribute('data-empty', 'yes');
-						this.point1.style.backgroundColor = null;
-						this.point1 = p1_toRight;
-						this.point1.setAttribute('data-empty', 'no');
-						this.point1.style.backgroundColor = '#874da8';
-						this.point2.setAttribute('data-empty', 'yes');
-						this.point2.style.backgroundColor = null;
-						this.point2 = p2_toRight;
-						this.point2.setAttribute('data-empty', 'no');
-						this.point2.style.backgroundColor = '#874da8';
+		}
+		if (this.position === 2) {
+			let x_p4 = +this.point4.getAttribute('data-x');
+			let y_p4 = +this.point4.getAttribute('data-y');
+			let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
+			let x_p2 = +this.point2.getAttribute('data-x');
+			let y_p2 = +this.point2.getAttribute('data-y');
+			let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
+			let x_p3 = +this.point3.getAttribute('data-x');
+			let y_p3 = +this.point3.getAttribute('data-y');
+			let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
+			let x_p1 = +this.point1.getAttribute('data-x');
+			let y_p1 = +this.point1.getAttribute('data-y');
+			let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
+			if (p1_toRight && p2_toRight && p3_toRight) {
+				if (
+					p1_toRight.getAttribute('data-empty') === 'yes' &&
+					p2_toRight.getAttribute('data-empty') === 'yes' &&
+					p3_toRight.getAttribute('data-empty') === 'yes'
+				) {
+					this.point1.setAttribute('data-empty', 'yes');
+					this.point1.style.backgroundColor = null;
+					this.point1 = p1_toRight;
+					this.point1.setAttribute('data-empty', 'no');
+					this.point1.style.backgroundColor = '#874da8';
+					this.point2.setAttribute('data-empty', 'yes');
+					this.point2.style.backgroundColor = null;
+					this.point2 = p2_toRight;
+					this.point2.setAttribute('data-empty', 'no');
+					this.point2.style.backgroundColor = '#874da8';
 
-						this.point3.setAttribute('data-empty', 'yes');
-						this.point3.style.backgroundColor = null;
-						this.point3 = p3_toRight;
-						this.point3.setAttribute('data-empty', 'no');
-						this.point3.style.backgroundColor = '#874da8';
-						this.point4.setAttribute('data-empty', 'yes');
-						this.point4.style.backgroundColor = null;
-						this.point4 = p4_toRight;
-						this.point4.setAttribute('data-empty', 'no');
-						this.point4.style.backgroundColor = '#874da8';
-					}
+					this.point3.setAttribute('data-empty', 'yes');
+					this.point3.style.backgroundColor = null;
+					this.point3 = p3_toRight;
+					this.point3.setAttribute('data-empty', 'no');
+					this.point3.style.backgroundColor = '#874da8';
+					this.point4.setAttribute('data-empty', 'yes');
+					this.point4.style.backgroundColor = null;
+					this.point4 = p4_toRight;
+					this.point4.setAttribute('data-empty', 'no');
+					this.point4.style.backgroundColor = '#874da8';
 				}
 			}
-			if (this.position === 3) {
-				let x_p4 = +this.point4.getAttribute('data-x');
-				let y_p4 = +this.point4.getAttribute('data-y');
-				let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
-				let x_p2 = +this.point2.getAttribute('data-x');
-				let y_p2 = +this.point2.getAttribute('data-y');
-				let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
-				let x_p3 = +this.point3.getAttribute('data-x');
-				let y_p3 = +this.point3.getAttribute('data-y');
-				let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
-				let x_p1 = +this.point1.getAttribute('data-x');
-				let y_p1 = +this.point1.getAttribute('data-y');
-				let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
-				if (p2_toRight && p4_toRight) {
-					if (
-						p2_toRight.getAttribute('data-empty') === 'yes' &&
-						p4_toRight.getAttribute('data-empty') === 'yes'
-					) {
-						this.point2.setAttribute('data-empty', 'yes');
-						this.point2.style.backgroundColor = null;
-						this.point2 = p2_toRight;
-						this.point2.setAttribute('data-empty', 'no');
-						this.point2.style.backgroundColor = '#874da8';
-						this.point4.setAttribute('data-empty', 'yes');
-						this.point4.style.backgroundColor = null;
-						this.point4 = p4_toRight;
-						this.point4.setAttribute('data-empty', 'no');
-						this.point4.style.backgroundColor = '#874da8';
-						this.point1.setAttribute('data-empty', 'yes');
-						this.point1.style.backgroundColor = null;
-						this.point1 = p1_toRight;
-						this.point1.setAttribute('data-empty', 'no');
-						this.point1.style.backgroundColor = '#874da8';
-						this.point3.setAttribute('data-empty', 'yes');
-						this.point3.style.backgroundColor = null;
-						this.point3 = p3_toRight;
-						this.point3.setAttribute('data-empty', 'no');
-						this.point3.style.backgroundColor = '#874da8';
-					}
+		}
+		if (this.position === 3) {
+			let x_p4 = +this.point4.getAttribute('data-x');
+			let y_p4 = +this.point4.getAttribute('data-y');
+			let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
+			let x_p2 = +this.point2.getAttribute('data-x');
+			let y_p2 = +this.point2.getAttribute('data-y');
+			let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
+			let x_p3 = +this.point3.getAttribute('data-x');
+			let y_p3 = +this.point3.getAttribute('data-y');
+			let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
+			let x_p1 = +this.point1.getAttribute('data-x');
+			let y_p1 = +this.point1.getAttribute('data-y');
+			let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
+			if (p2_toRight && p4_toRight) {
+				if (
+					p2_toRight.getAttribute('data-empty') === 'yes' &&
+					p4_toRight.getAttribute('data-empty') === 'yes'
+				) {
+					this.point2.setAttribute('data-empty', 'yes');
+					this.point2.style.backgroundColor = null;
+					this.point2 = p2_toRight;
+					this.point2.setAttribute('data-empty', 'no');
+					this.point2.style.backgroundColor = '#874da8';
+					this.point4.setAttribute('data-empty', 'yes');
+					this.point4.style.backgroundColor = null;
+					this.point4 = p4_toRight;
+					this.point4.setAttribute('data-empty', 'no');
+					this.point4.style.backgroundColor = '#874da8';
+					this.point1.setAttribute('data-empty', 'yes');
+					this.point1.style.backgroundColor = null;
+					this.point1 = p1_toRight;
+					this.point1.setAttribute('data-empty', 'no');
+					this.point1.style.backgroundColor = '#874da8';
+					this.point3.setAttribute('data-empty', 'yes');
+					this.point3.style.backgroundColor = null;
+					this.point3 = p3_toRight;
+					this.point3.setAttribute('data-empty', 'no');
+					this.point3.style.backgroundColor = '#874da8';
 				}
 			}
-			if (this.position === 4) {
-				let x_p4 = +this.point4.getAttribute('data-x');
-				let y_p4 = +this.point4.getAttribute('data-y');
-				let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
-				let x_p2 = +this.point2.getAttribute('data-x');
-				let y_p2 = +this.point2.getAttribute('data-y');
-				let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
-				let x_p3 = +this.point3.getAttribute('data-x');
-				let y_p3 = +this.point3.getAttribute('data-y');
-				let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
-				let x_p1 = +this.point1.getAttribute('data-x');
-				let y_p1 = +this.point1.getAttribute('data-y');
-				let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
-				if (p2_toRight && p3_toRight && p4_toRight) {
-					if (
-						p2_toRight.getAttribute('data-empty') === 'yes' &&
-						p3_toRight.getAttribute('data-empty') === 'yes' &&
-						p4_toRight.getAttribute('data-empty') === 'yes'
-					) {
-						this.point4.setAttribute('data-empty', 'yes');
-						this.point4.style.backgroundColor = null;
-						this.point4 = p4_toRight;
-						this.point4.setAttribute('data-empty', 'no');
-						this.point4.style.backgroundColor = '#874da8';
-						this.point2.setAttribute('data-empty', 'yes');
-						this.point2.style.backgroundColor = null;
-						this.point2 = p2_toRight;
-						this.point2.setAttribute('data-empty', 'no');
-						this.point2.style.backgroundColor = '#874da8';
+		}
+		if (this.position === 4) {
+			let x_p4 = +this.point4.getAttribute('data-x');
+			let y_p4 = +this.point4.getAttribute('data-y');
+			let p4_toRight = document.querySelectorAll(`[data-y='${y_p4}']`)[x_p4 + 1];
+			let x_p2 = +this.point2.getAttribute('data-x');
+			let y_p2 = +this.point2.getAttribute('data-y');
+			let p2_toRight = document.querySelectorAll(`[data-y='${y_p2}']`)[x_p2 + 1];
+			let x_p3 = +this.point3.getAttribute('data-x');
+			let y_p3 = +this.point3.getAttribute('data-y');
+			let p3_toRight = document.querySelectorAll(`[data-y='${y_p3}']`)[x_p3 + 1];
+			let x_p1 = +this.point1.getAttribute('data-x');
+			let y_p1 = +this.point1.getAttribute('data-y');
+			let p1_toRight = document.querySelectorAll(`[data-y='${y_p1}']`)[x_p1 + 1];
+			if (p2_toRight && p3_toRight && p4_toRight) {
+				if (
+					p2_toRight.getAttribute('data-empty') === 'yes' &&
+					p3_toRight.getAttribute('data-empty') === 'yes' &&
+					p4_toRight.getAttribute('data-empty') === 'yes'
+				) {
+					this.point4.setAttribute('data-empty', 'yes');
+					this.point4.style.backgroundColor = null;
+					this.point4 = p4_toRight;
+					this.point4.setAttribute('data-empty', 'no');
+					this.point4.style.backgroundColor = '#874da8';
+					this.point2.setAttribute('data-empty', 'yes');
+					this.point2.style.backgroundColor = null;
+					this.point2 = p2_toRight;
+					this.point2.setAttribute('data-empty', 'no');
+					this.point2.style.backgroundColor = '#874da8';
 
-						this.point3.setAttribute('data-empty', 'yes');
-						this.point3.style.backgroundColor = null;
-						this.point3 = p3_toRight;
-						this.point3.setAttribute('data-empty', 'no');
-						this.point3.style.backgroundColor = '#874da8';
+					this.point3.setAttribute('data-empty', 'yes');
+					this.point3.style.backgroundColor = null;
+					this.point3 = p3_toRight;
+					this.point3.setAttribute('data-empty', 'no');
+					this.point3.style.backgroundColor = '#874da8';
 
-						this.point1.setAttribute('data-empty', 'yes');
-						this.point1.style.backgroundColor = null;
-						this.point1 = p1_toRight;
-						this.point1.setAttribute('data-empty', 'no');
-						this.point1.style.backgroundColor = '#874da8';
-					}
+					this.point1.setAttribute('data-empty', 'yes');
+					this.point1.style.backgroundColor = null;
+					this.point1 = p1_toRight;
+					this.point1.setAttribute('data-empty', 'no');
+					this.point1.style.backgroundColor = '#874da8';
 				}
 			}
-		};
+		}
 	};
 	return shapeTriangle;
-})();
+}
 
 function twoCellShape(x = 3, y = 3) {
 	let twoCell = {};
